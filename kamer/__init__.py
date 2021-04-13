bf1f60c0bf0d37c13e061f0fd5f54415787e9567
Bart Thate <bthate@dds.nl>
2021-04-13 15:23:22 +0200

    lijdend voorwerp

--- kamer/__init__.py
+++
@@ -1,8 +0,0 @@
-# kamer/__init__.py
-#
-#
-
-""" kamer package. """
-
-__version__ = 20
-__txt__ = "using the law to adminster poison the king commits genocide"
--- kamer/ver.py
+++ kamer/ver.py
@@ -4,7 +4,7 @@
 
 """ version plugin. """
 
-from kamer import __version__
+__version__ = 22
 
 txt = "OTP-CR-117/19 otp.informationdesk@icc-cpi.int http://pypi.org/project/genocide !"
 
